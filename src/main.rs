7fdd78471b58e92a5e6dd5970a2f5de577dea90c
kurorinchan <kurorinchan@users.noreply.github.com>
2024-02-08 19:24:41 +0900

    Moved simulation code to separate file

--- src/main.rs
+++ src/main.rs
@@ -154,12 +154,10 @@ impl Simulation {
         })
     }
 
-    pub fn position_of(&self, buff:&Buff) -> Option<usize> {
-        self.buffs.iter().position(|state| {
-            match state {
-                SlotState::Free(b) | SlotState::Locked(b) => b.eq(buff),
-                _ => false,
-            }
+    pub fn position_of(&self, buff: &Buff) -> Option<usize> {
+        self.buffs.iter().position(|state| match state {
+            SlotState::Free(b) | SlotState::Locked(b) => b.eq(buff),
+            _ => false,
         })
     }
 
@@ -278,177 +276,16 @@ fn simulate_once() -> Vec<Option<Buff>> {
     drawn_buffs
 }
 
-fn simulation_results_to_buffs(sim_results: &[Option<Buff>]) -> Vec<Buff> {
-    sim_results.iter().filter_map(Option::clone).collect()
-}
-
-// This is more of a check/test than a simluation.
-// This verifies that the distribution of the slots being populated matches the expected.
-// Only one (which is the first slot)  populated 35%.
-// Any two slots populated is 50%.
-// All two slots populated is 15%.
-pub fn simulation_slots_shown_distribution() {
-    let attempts = 100000;
-
-    let mut tally = [0, 0, 0];
-    for _ in 0..attempts {
-        let result = simulate_once();
-        let result = simulation_results_to_buffs(&result);
-        tally[result.len() - 1] += 1;
-    }
-
-    println!("Tally of buff nums! {:?}", tally);
-    let sum: usize = tally.iter().sum();
-
-    let one = tally[0] as f64 / sum as f64 * 100.0;
-    let two = tally[1] as f64 / sum as f64 * 100.0;
-    let three = tally[2] as f64 / sum as f64 * 100.0;
-
-    println!("Percentage of buffs {:.2}% {:.2}% {:.2}%", one, two, three);
-}
-
-// The number of attempts required to get all the wanted buffs, without locking.
-// For example if you want Attack and ChargeSpeed. This simulation checks how many rolls
-// got you all the buffs that you want. Divide that number by the total attempts should get us
-// the probability.
-fn simulation_want_specific() {
-    let want: HashSet<Buff> = [Buff::Attack, Buff::ChargeSpeed].into_iter().collect();
-    let attempts = 100000;
-
-    let mut hits = 0;
-    for _ in 0..attempts {
-        let result = simulate_once();
-        let result = simulation_results_to_buffs(&result);
-        let result: Vec<&Buff> = result
-            .into_iter()
-            .filter_map(|item| want.get(&item))
-            .collect();
-        if result.len() == want.len() {
-            hits += 1;
-        }
-    }
-    println!(
-        "Out of {attempts}, {hits} containted the wanted buffs. That is {}%",
-        hits as f64 / attempts as f64 * 100.0
-    );
-}
-
-fn reroll_until_all_found(sim: &mut Simulation, want: &HashSet<Buff>) {
-    loop {
-        sim.reroll();
-        let mut found = HashSet::new();
-
-        for b in want.iter() {
-            if sim.has_buff(b) {
-                found.insert(b.clone());
-            }
-        }
-
-        if want.eq(&found) {
-            break;
-        }
-    }
-}
-
-fn reroll_until_all_found_with_locking(sim: &mut Simulation, want: &HashSet<Buff>) {
-    loop {
-        sim.reroll();
-        let mut found = HashSet::new();
-
-        for b in want.iter() {
-            if sim.has_buff(b) {
-                let pos = sim.position_of(b).unwrap();
-                //if pos == 0 {
-                    sim.lock(pos);
-                //}
-                found.insert(b.clone());
-            }
-        }
-
-        if want.eq(&found) {
-            break;
-        }
-    }
-}
-
-// Simulation to see how many custom modules are required to get all buffs without locking.
-// For example if you want Attack and ChargeSpeed. This simulation checks how many custom modules
-// are used to got you all the buffs that you want.
-// This runs the process multiple times and gets the average number of modules required, without
-// locking.
-// Oddly this does not match with the simulation that gets the probability of getting all buffs.
-fn simulation_num_cus_mod_for_specific() {
-    let want: HashSet<Buff> = HashSet::from_iter([Buff::Attack, Buff::MaxAmmo]);
-    let attempts = 100000;
-
-    let mut sum_custom_mods = 0;
-
-    for _ in 0..attempts {
-        let mut sim = Simulation::new();
-        reroll_until_all_found(&mut sim, &want);
-        sum_custom_mods += sim.custom_modules;
-    }
-
-    println!(
-        "To get all {:?} for {attempts} times, it required {} custom mods. That is on average {} mods per success.",
-        want, sum_custom_mods, 
-        sum_custom_mods as f64 / attempts as f64
-    );
-}
-
-// Simulate to see how many custom modules are required to get a specific set of buffs, with locking.
-fn simulation_num_cus_mods_with_locking() {
-    let want: HashSet<Buff> = HashSet::from_iter([Buff::Attack, Buff::MaxAmmo]);
-    let attempts = 100000;
-
-    let mut sum_custom_mods = 0;
-
-    for _ in 0..attempts {
-        let mut sim = Simulation::new();
-        reroll_until_all_found_with_locking(&mut sim, &want);
-        sum_custom_mods += sim.custom_modules;
-    }
-
-    println!(
-        "To get all {:?} for {attempts} times, it required {} custom mods. That is on average {} mods per success.",
-        want, sum_custom_mods, 
-        sum_custom_mods as f64 / attempts as f64
-    );
-}
-
-fn simulation_num_cus_mods_with_certain_buffs_already_locked() {
-    let want: HashSet<Buff> = HashSet::from_iter([Buff::Attack, Buff::Elemental, Buff::MaxAmmo]);
-    let attempts = 100000;
-
-    let mut sum_custom_mods = 0;
-
-    for _ in 0..attempts {
-        let mut sim = Simulation::new();
-        sim.set_buff(0, &Buff::Attack);
-        sim.lock_first();
-
-        reroll_until_all_found_with_locking(&mut sim, &want);
-        sum_custom_mods += sim.custom_modules;
-    }
-
-    println!(
-        "To get all {:?} for {attempts} times, it required {} custom mods. That is on average {} mods per success.",
-        want, sum_custom_mods, 
-        sum_custom_mods as f64 / attempts as f64
-    );
-}
-
-
 fn main() {
-    simulation_slots_shown_distribution();
+    simulation::simulation_slots_shown_distribution();
     println!("sim want specific");
-    simulation_want_specific();
+    simulation::simulation_want_specific();
     println!("sim num cusmods");
-    simulation_num_cus_mod_for_specific();
+    simulation::simulation_num_cus_mod_for_specific();
     println!("sim num cusmods with locking");
-    simulation_num_cus_mods_with_locking();
-    println!("sim num cusmods with certain set, with locking");
-    simulation_num_cus_mods_with_certain_buffs_already_locked();
+    simulation::simulation_num_cus_mods_with_locking();
+    println!("sim first desired buff locked");
+    simulation::simulation_first_desired_buff_locked();
 }
 
 #[cfg(test)]
@@ -649,7 +486,7 @@ mod test {
     fn locking_should_use_more_custom_modules() {
         let mut sim = Simulation::new();
         sim.reroll();
-        
+
         assert_eq!(sim.custom_modules, 1);
 
         sim.lock_first();
@@ -663,7 +500,7 @@ mod test {
     fn locking_should_use_more_custom_modules_locking_two_slots() {
         let mut sim = Simulation::new();
         sim.reroll();
-        
+
         assert_eq!(sim.custom_modules, 1);
         sim.lock_first();
 
--- src/simulation.rs
+++ src/simulation.rs
@@ -1,1 +1,162 @@
 
+use crate::*;
+
+fn simulation_results_to_buffs(sim_results: &[Option<Buff>]) -> Vec<Buff> {
+    sim_results.iter().filter_map(Option::clone).collect()
+}
+
+// This is more of a check/test than a simluation.
+// This verifies that the distribution of the slots being populated matches the expected.
+// Only one (which is the first slot)  populated 35%.
+// Any two slots populated is 50%.
+// All two slots populated is 15%.
+pub fn simulation_slots_shown_distribution() {
+    let attempts = 100000;
+
+    let mut tally = [0, 0, 0];
+    for _ in 0..attempts {
+        let result = simulate_once();
+        let result = simulation_results_to_buffs(&result);
+        tally[result.len() - 1] += 1;
+    }
+
+    println!("Tally of buff nums! {:?}", tally);
+    let sum: usize = tally.iter().sum();
+
+    let one = tally[0] as f64 / sum as f64 * 100.0;
+    let two = tally[1] as f64 / sum as f64 * 100.0;
+    let three = tally[2] as f64 / sum as f64 * 100.0;
+
+    println!("Percentage of buffs {:.2}% {:.2}% {:.2}%", one, two, three);
+}
+
+// The number of attempts required to get all the wanted buffs, without locking.
+// For example if you want Attack and ChargeSpeed. This simulation checks how many rolls
+// got you all the buffs that you want. Divide that number by the total attempts should get us
+// the probability.
+pub fn simulation_want_specific() {
+    let want: HashSet<Buff> = [Buff::Attack, Buff::ChargeSpeed].into_iter().collect();
+    let attempts = 100000;
+
+    let mut hits = 0;
+    for _ in 0..attempts {
+        let result = simulate_once();
+        let result = simulation_results_to_buffs(&result);
+        let result: Vec<&Buff> = result
+            .into_iter()
+            .filter_map(|item| want.get(&item))
+            .collect();
+        if result.len() == want.len() {
+            hits += 1;
+        }
+    }
+    println!(
+        "Out of {attempts}, {hits} containted the wanted buffs. That is {}%",
+        hits as f64 / attempts as f64 * 100.0
+    );
+}
+
+fn reroll_until_all_found(sim: &mut Simulation, want: &HashSet<Buff>) {
+    loop {
+        sim.reroll();
+        let mut found = HashSet::new();
+
+        for b in want.iter() {
+            if sim.has_buff(b) {
+                found.insert(b.clone());
+            }
+        }
+
+        if want.eq(&found) {
+            break;
+        }
+    }
+}
+
+fn reroll_until_all_found_with_locking(sim: &mut Simulation, want: &HashSet<Buff>) {
+    loop {
+        sim.reroll();
+        let mut found = HashSet::new();
+
+        for b in want.iter() {
+            if sim.has_buff(b) {
+                let pos = sim.position_of(b).unwrap();
+                sim.lock(pos);
+                found.insert(b.clone());
+            }
+        }
+
+        if want.eq(&found) {
+            break;
+        }
+    }
+}
+
+// Simulation to see how many custom modules are required to get all buffs without locking.
+// For example if you want Attack and ChargeSpeed. This simulation checks how many custom modules
+// are used to got you all the buffs that you want.
+// This runs the process multiple times and gets the average number of modules required, without
+// locking.
+// Oddly this does not match with the simulation that gets the probability of getting all buffs.
+pub fn simulation_num_cus_mod_for_specific() {
+    let want: HashSet<Buff> = HashSet::from_iter([Buff::Attack, Buff::MaxAmmo]);
+    let attempts = 100000;
+
+    let mut sum_custom_mods = 0;
+
+    for _ in 0..attempts {
+        let mut sim = Simulation::new();
+        reroll_until_all_found(&mut sim, &want);
+        sum_custom_mods += sim.custom_modules;
+    }
+
+    println!(
+        "To get all {:?} for {attempts} times, it required {} custom mods. That is on average {} mods per success.",
+        want, sum_custom_mods, 
+        sum_custom_mods as f64 / attempts as f64
+    );
+}
+
+// Simulate to see how many custom modules are required to get a specific set of buffs, with locking.
+pub fn simulation_num_cus_mods_with_locking() {
+    let want: HashSet<Buff> = HashSet::from_iter([Buff::Attack, Buff::MaxAmmo]);
+    let attempts = 100000;
+
+    let mut sum_custom_mods = 0;
+
+    for _ in 0..attempts {
+        let mut sim = Simulation::new();
+        reroll_until_all_found_with_locking(&mut sim, &want);
+        sum_custom_mods += sim.custom_modules;
+    }
+
+    println!(
+        "To get all {:?} for {attempts} times, it required {} custom mods. That is on average {} mods per success.",
+        want, sum_custom_mods, 
+        sum_custom_mods as f64 / attempts as f64
+    );
+}
+
+// Find custom module usage given that a desired buff is locked on the firsr slot.
+pub fn simulation_first_desired_buff_locked() {
+    let want: HashSet<Buff> = HashSet::from_iter([Buff::Attack, Buff::Elemental, Buff::MaxAmmo]);
+    let attempts = 100000;
+
+    let mut sum_custom_mods = 0;
+
+    for _ in 0..attempts {
+        let mut sim = Simulation::new();
+        sim.set_buff(0, &Buff::Attack);
+        sim.lock_first();
+
+        reroll_until_all_found_with_locking(&mut sim, &want);
+        sum_custom_mods += sim.custom_modules;
+    }
+
+    println!(
+        "To get all {:?} for {attempts} times, it required {} custom mods. That is on average {} mods per success.",
+        want, sum_custom_mods, 
+        sum_custom_mods as f64 / attempts as f64
+    );
+}
+
